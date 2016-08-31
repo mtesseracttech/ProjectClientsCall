e319f4e3754cd72cc9157b8e4699c2083d24a8d8
Ruud Peters <ruud_peters96@hotmail.com>
2016-08-31 16:34:02 +0200

    Statemachine

--- Assets/Scripts/Player & Camera/MovementScript.cs
+++ Assets/Scripts/Player & Camera/MovementScript.cs
@@ -1,12 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using StateMachine;
 
 public class MovementScript : MonoBehaviour {
 
     public float speed;
-    public float rayTollerance = 1;
+    public float floatHeight;
+    public float gravity;
+    public float jumpHeight;
+
+    private SquirrelMachine s;
+
+    private Vector2 velocity;
+    private Vector2 acceleration;
 
     private BoxCollider playerCollider;
+
+    private event Movement;
+    private delegate void Movement();
 
 	void Start () {
 
@@ -14,7 +25,18 @@ public class MovementScript : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        if (s.HasChanged() == true)
+        {
+            switch(s.CurrentState)
+            {
+                case SquirrelState.running:
+                    print(true);
+                    break;
+            }
+        }
+
         Movement();
         CheckCollision();
 
@@ -22,45 +44,41 @@ public class MovementScript : MonoBehaviour {
 
     void CheckCollision()
     {
+        transform.position += new Vector3(velocity.x, 0, 0);
+
         Vector3 bottomRayPos = this.transform.up * playerCollider.size.y / 2;
 
         Vector3 frontRayPos = this.transform.position + this.transform.right * (playerCollider.size.x / 2) - bottomRayPos;
         Vector3 rearRayPos = this.transform.position - this.transform.right * (playerCollider.size.x / 2) - bottomRayPos;
 
-        Ray frontRay = new Ray(frontRayPos, Vector3.down);
-        Ray rearRay = new Ray(rearRayPos, Vector3.down);
+        Ray frontRay = new Ray(frontRayPos, -transform.up);
+        Ray rearRay = new Ray(rearRayPos, -transform.up);
 
         Debug.DrawRay(rearRayPos, Vector3.down, Color.red);
         Debug.DrawRay(frontRayPos, Vector3.down, Color.green);
 
         RaycastHit frontHit;
         RaycastHit rearHit;
-        
-        if (Physics.Raycast(frontRay, out frontHit, rayTollerance) && Physics.Raycast(rearRay, out rearHit, rayTollerance))
+
+        if (Physics.Raycast(frontRay, out frontHit, floatHeight * 2) && Physics.Raycast(rearRay, out rearHit, floatHeight * 2))
         {
-            if (Vector3.Angle(frontHit.normal, rearHit.normal) > 0.1f)
-            {
-                transform.eulerAngles = new Vector3(0, 0, Mathf.Asin((frontHit.point.y - rearHit.point.y) / playerCollider.size.x) * Mathf.Rad2Deg);
-                print(transform.eulerAngles.z);
-            }
-            else
-            {
-                if()
-            }
-            
+            transform.eulerAngles = new Vector3(0, 0, Mathf.Asin((frontHit.point.y - rearHit.point.y) / playerCollider.size.x) * Mathf.Rad2Deg);
+
+            float currentHeight;
+            if ((currentHeight = (frontHit.distance + rearHit.distance) / 2) < floatHeight)
+                transform.position += (floatHeight - currentHeight) * transform.up;
         }
+
+
+        acceleration = new Vector2();
     }
 
-    void Movement()
-    {
-        if (Input.GetKey(KeyCode.W))
-            transform.position += new Vector3(0, speed, 0);
-        else if (Input.GetKey(KeyCode.S))
-            transform.position -= new Vector3(0, speed, 0);
 
-        if (Input.GetKey(KeyCode.A))
-            transform.position -= transform.right * speed;
-        if (Input.GetKey(KeyCode.D))
-            transform.position += transform.right * speed;
-    }
+    //void Movement()
+    //{
+    //    if (Input.GetKey(KeyCode.W))
+    //        s.MoveNext(Key.up);
+    //    if (Input.GetKey(KeyCode.S))
+    //        s.MoveNext(Key.down);
+    //}
 }
---
+++ Assets/Scripts/Player & Camera/StateMachine.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+public enum SquirrelState
+{
+    idling,
+    running,
+    jumping,
+    climbing,
+    flying,
+    landing,
+}
+
+public enum Orientation //for later state
+{
+    left,
+    right
+}
+
+public enum Key
+{
+    up,
+    down,
+}
+
+namespace StateMachine
+{
+    public class SquirrelMachine
+    {
+        class StateTransition
+        {
+            readonly SquirrelState CurrentState;
+            readonly Key Command;
+
+            public StateTransition(SquirrelState currentState, Key command)
+            {
+                CurrentState = currentState;
+                Command = command;
+            }
+
+            public override int GetHashCode()
+            {
+                return 17 + 31 * CurrentState.GetHashCode() + 31 * Command.GetHashCode();
+            }
+
+            public override bool Equals(object obj)
+            {
+                StateTransition other = obj as StateTransition;
+                return other != null && this.CurrentState == other.CurrentState && this.Command == other.Command;
+            }
+        }
+
+        Dictionary<StateTransition, SquirrelState> transitions;
+        public SquirrelState CurrentState { get; private set; }
+        private SquirrelState OldState;
+
+        public SquirrelMachine()
+        {
+            CurrentState = SquirrelState.idling;
+            OldState = CurrentState;
+
+            transitions = new Dictionary<StateTransition, SquirrelState>
+            {
+                { new StateTransition(SquirrelState.idling, Key.up), SquirrelState.jumping },
+                { new StateTransition(SquirrelState.running, Key.up), SquirrelState.jumping }
+            };
+        }
+
+        public SquirrelState GetNext(Key command)
+        {
+            StateTransition transition = new StateTransition(CurrentState, command);
+            SquirrelState nextState;
+            if (!transitions.TryGetValue(transition, out nextState))
+                throw new Exception("Invalid transition: " + CurrentState + " -> " + command);
+            return nextState;
+        }
+
+        public SquirrelState MoveNext(Key command)
+        {
+            CurrentState = GetNext(command);
+            return CurrentState;
+        }
+
+        public bool HasChanged()
+        {
+            if (CurrentState != OldState)
+            {
+                OldState = CurrentState;
+                return true;
+            }
+            return false;
+        }
+    }
+}
