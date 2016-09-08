1292d13c0833ef5552f8cd10035f1d7967324dcc
Kotryna <kaimelisvy24@outlook.com>
2016-09-08 19:46:48 +0300

    fixed stuff. you can finally get to fifth day. but not win or loose :D

--- Assets/Scripts/Environment/FeedingBehavior.cs
+++ Assets/Scripts/Environment/FeedingBehavior.cs
@@ -12,6 +12,11 @@ public class FeedingBehavior : MonoBehaviour
     private bool _3rdDayAmountOfAcorn = false;
     private bool _4thDayAmountOfAcorn = false;
     private bool _5thDayAmountOfAcorn = false;
+    private bool _1stDayPassed = false;
+    private bool _2ndDayPassed = false;
+    private bool _3rdDayPassed = false;
+    private bool _4thDayPassed = false;
+    private bool _5thDayPassed = false;
 
     public int maxAmount;
     public Inventory HaveAcorn;
@@ -44,6 +49,14 @@ public class FeedingBehavior : MonoBehaviour
         }
     }
 
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player" )
+        {
+            InTheCave = false;
+        }
+    }
+
     private void DayChecking(bool first, bool second, bool third, bool fourth, bool fifth, bool all)
     {
         _1stDayAmountOfAcorn = first;
@@ -54,78 +67,93 @@ public class FeedingBehavior : MonoBehaviour
         haveEnoguhAcorns = all;
     }
 
+    private void CheckingDaysPassed(bool first, bool second, bool third, bool fourth, bool fifth)
+    {
+        _1stDayPassed = first;
+        _2ndDayPassed = second;
+        _3rdDayPassed = third;
+        _4thDayPassed = fourth;
+        _5thDayPassed = fifth;
+    }
+
     private void CheckingTheDay()
     {
         if (HaveAcorn.GotAcorn)
         {
-            if (HaveAcorn.NutCount == FirstDayAcorn)
+            if (HaveAcorn.NutCount == FirstDayAcorn && !_1stDayPassed)
             {
                DayChecking(true,false,false,false,false,true);
+                CheckingDaysPassed(true,false,false,false,false);
                 Debug.Log("we have for first day");
             }
-            else if(HaveAcorn.NutCount == SecondDayAcorn)
+            else if(HaveAcorn.NutCount == SecondDayAcorn && _1stDayPassed)
             {
                 DayChecking(false, true, false, false, false,true);
+                CheckingDaysPassed(true, true, false, false, false);
                 Debug.Log("we have for second day");
             }
-            else if (HaveAcorn.NutCount == ThirdDayAcorn)
+            else if (HaveAcorn.NutCount == ThirdDayAcorn && _2ndDayPassed)
             {
                 DayChecking(false, false, true, false, false,true);
+                CheckingDaysPassed(true, true, true, false, false);
                 Debug.Log("we have for third day");
             }
-            else if (HaveAcorn.NutCount == FourthDayAcorn)
+            else if (HaveAcorn.NutCount == FourthDayAcorn && _3rdDayPassed)
             {
                 DayChecking(false, false, false, true, false,true);
+                CheckingDaysPassed(true, true, true, true, false);
                 Debug.Log("we have for fourth day");
             }
-            else if (HaveAcorn.NutCount == FifthDayAcorn)
+            else if (HaveAcorn.NutCount == FifthDayAcorn && _4thDayPassed)
             {
                 DayChecking(false, false, false, false, true, true);
+                CheckingDaysPassed(true, true, true, true, true);
                 Debug.Log("we have for fifth day");
-            }
-            else
-            {
-                DayChecking(false, false, false, false, false, false);
             }
         }
     }
 
     private void ResetingDay()
     {
+        
+
         if (!Cycle.NightHasCome && Cycle.DaysHasPassed == 0 && _1stDayAmountOfAcorn && InTheCave)
         {
             HaveAcorn.NutCount = 0;
-            DayChecking(false, false, false, false, false, true);
             Debug.Log("1st day");
+            DayChecking(false, false, false, false, false, false);
             maxAmount = SecondDayAcorn;
-
         }
         else if (!Cycle.NightHasCome && Cycle.DaysHasPassed == 1 && _2ndDayAmountOfAcorn && InTheCave)
         {
             HaveAcorn.NutCount = 0;
-            DayChecking(false, false, false, false, false, true);
             Debug.Log("2nd day");
+            DayChecking(false, false, false, false, false, false);
             maxAmount = ThirdDayAcorn;
+
         }
         else if(!Cycle.NightHasCome && Cycle.DaysHasPassed == 2 && _3rdDayAmountOfAcorn && InTheCave)
         {
             HaveAcorn.NutCount = 0;
-            DayChecking(false, false, false, false, false, true);
             Debug.Log("3rd day");
+            DayChecking(false, false, false, false, false, false);
             maxAmount = FourthDayAcorn;
+
         }
         else if (!Cycle.NightHasCome && Cycle.DaysHasPassed == 3 && _4thDayAmountOfAcorn && InTheCave)
         {
             HaveAcorn.NutCount = 0;
-            DayChecking(false, false, false, false, false, true);
             Debug.Log("4th day");
+            DayChecking(false, false, false, false, false, false);
             maxAmount = FifthDayAcorn;
+
         }
         else if (!Cycle.NightHasCome && Cycle.DaysHasPassed == 4 && _5thDayAmountOfAcorn && InTheCave)
         {
+            DayChecking(false, false, false, false, false, false);
             HaveAcorn.NutCount = 0;
-            DayChecking(false, false, false, false, false, true);
             Debug.Log("5th day");
+
         }
     }
 }
--- Assets/Scripts/Menu/UI/UIsun.cs
+++ Assets/Scripts/Menu/UI/UIsun.cs
@@ -6,30 +6,11 @@ using UnityEngine.UI;
 public class UIsun : MonoBehaviour
 {
     public Image SunProgressBar;
-   // public float decreaseSpeed = 0.016f;
     public DayNightCycles dayspeed;
-    private float sunAngleRotationSpeed;
-    private float speedOfRotation;
-    void Start()
-    {
-        sunAngleRotationSpeed = dayspeed.DayRotateSpeed.x;//-1
-       // speedOfRotation = dayspeed.SpeedPerTime();//~0.016
-        speedOfRotation = Time.deltaTime;
-    }
 
     void Update()
     {
-        if (!dayspeed.NightHasCome)
-        {
             
-            SunProgressBar.fillAmount -= ((sunAngleRotationSpeed * speedOfRotation) / 220);
-
-        }
-        else
-
-        {
-            Debug.Log("fill amount ran out");
-        }
-
+      SunProgressBar.fillAmount -= (dayspeed.Speed / 198);
     }
 }
